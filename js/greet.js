b3aac032bf9fda52b764fa2c8a11c49bfaed8646
Sibabalwe-Qamata <prof.qamata@gmail.com>
2018-05-12 04:10:55 +0200

    Refactored the factory function to store names in local storage

--- js/greet.js
+++ js/greet.js
@@ -1,55 +1,60 @@
-function greeting() {
+function greeting(UserDatabase) 
+{
 
-    var counter = 0;
+    var Name_to_greet = '';
+    var language= '';
+    var namesGreeted = {};
 
+    
+    function setName(greetName){
+        if(greetName !== " "){Name_to_greet = greetName;}
+    }
 
-    //Needs to take a Map
+    function setLanguage(lang){language= lang;}
 
+    function checkUserStoredList(UserDatabase){
+        if(UserDatabase){ namesGreeted = UserDatabase;}
 
-
-    function greetMe(greetMap) {
-        if (){
-
-
-        } else if () {
-            
-
-        } else if () {
-
+        if(Name_to_greet !==""){
+            if(namesGreeted[Name_to_greet] === undefined)
+            {
+                namesGreeted[Name_to_greet]=0;
+            }
         }
 
-
-
     }
 
-    /**function greetIsixhosa(isixhosaGreet) {
-        var greetOne = "Molo, " + isixhosaGreet;
-        counter++;
-        return greetOne;
-    }
-    function greetEnglish(englishGreet) {
-        var greetTwo = "Hello, " + englishGreet;
-        counter++;
-        return greetTwo;
-    }
-    function greetAfrikaans(afrikaansGreet) {
-        var greetThree = "Hallo, " + afrikaansGreet;
-        counter++;
-        return greetThree;
-    }   **/
-
-
-    function peopleGreeted() {
-        return counter;
+    function greetUser()
+    {
+        if(language === "Isixhosa"){return "Molo, "+Name_to_greet;}
+        else if(language === "Afrikaans"){return "Hallo, "+Name_to_greet;}
+        else if(language ==="English"){return "Molo, "+Name_to_greet;}
     }
 
+    //Below are Getter functions
+
+    function getNameToGreet(){return Name_to_greet;}
+
+    function getLanguageChoice(){return language;}
+
+    function getNameMap(){return namesGreeted;}
+
+    function updateNameMap(){return namesGreeted ={};}
+
+    function getCounter(){return Object.keys(namesGreeted).length;}
 
     return {
-        isixhosa: greetIsixhosa,
-        english: greetEnglish,
-        afrikaans: greetAfrikaans,
-        people: peopleGreeted
+        setname: setName,
+        set_language: setLanguage,
+        checkList: checkUserStoredList,
 
+        get_name : getNameToGreet,
+        get_language : getLanguageChoice,
+        get_NameList : getNameMap,
+
+        doGreet : greetUser,
+        counter : getCounter,
+        updateNameList : updateNameMap
     }
 
 }
