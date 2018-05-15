81d51d987273f308f98ec6c33d0236b7dd2e1fee
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-05-15 16:23:41 +0200

    Added To-Do comment

--- js/GreetDom.js
+++ js/GreetDom.js
@@ -1,81 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    //Below is the Factory Function
-
-
-    /**function greeting(UserDatabase) {
-
-        var Name_to_greet = '';
-        var language = '';
-        var Counter = 0;
-        var namesGreeted = UserDatabase || {};
-
-
-        function setName(greetName) {
-            if (greetName !== " ") {
-                Name_to_greet = greetName;
-            }
-
-            if (namesGreeted[Name_to_greet] === undefined) 
-            {
-                namesGreeted[Name_to_greet] = 0;
-            }
-        }
-
-        function setLanguage(lang) {
-            language = lang;
-        }
-
-
-
-        function greetUser() {
-            if (language === "Isixhosa") {
-                return "Molo, " + Name_to_greet;
-            } else if (language === "Afrikaans") {
-                return "Hallo, " + Name_to_greet;
-            } else if (language === "English") {
-                return "Hello, " + Name_to_greet;
-            }
-        }
-
-        //Below are Getter functions
-
-        function getNameToGreet() {
-            return Name_to_greet;
-        }
-
-
-
-        function getLanguageChoice() {
-            return language;
-        }
-
-        function getNameMap() {
-            return namesGreeted;
-        }
-
-        function getCounter() {
-            return Object.keys(namesGreeted).length;
-        }
-        
-        
-         return {
-            set_Name: setName,
-            set_language: setLanguage,
-
-
-            get_name: getNameToGreet,
-            get_language: getLanguageChoice,
-            get_NameList: getNameMap, 
-            doGreet: greetUser,
-            counter: getCounter
-        }
-
-
-    }**/
-
-    //----------------------------------------End Here------------------------------------------------//
-
     //get a reference to the billString
     var greetingText = document.querySelector(".text-name");
 
@@ -88,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var DisplayGreet = document.querySelector(".writer");
    
-    //var GreetMe = greeting();
 
     //Below to get the stored users from local storage
     var storedUserList = localStorage.getItem('Name') ? JSON.parse(localStorage.getItem('Name')) : {};
@@ -108,19 +31,22 @@ document.addEventListener('DOMContentLoaded', function () {
         var languageType = checkedRadioBtn.value;
         var nameOfPerson = greetingText.value.trim();
 
-       
-        // Variables to hold the values of inputs
-
+        /***ToDo
+         * Handle User Input 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * ***/
         if(nameOfPerson === nameOfPerson.toLowerCase())
         {
-            //var CapitalizeInput = capitalizeFirstLetter(nameOfPerson);
             var nameToset = namesToStore.set_Name(nameOfPerson);
             var languageToUse = namesToStore.set_language(languageType);
         }
         else if(nameOfPerson === nameOfPerson.toUpperCase())
         {
-            //var putLowerCase = nameOfPerson.toLowerCase();
-            //var CapitalizeInput = capitalizeFirstLetter(putLowerCase);
+        
             var nameToset = namesToStore.set_Name(nameOfPerson);
             var languageToUse = namesToStore.set_language(languageType);
         }
